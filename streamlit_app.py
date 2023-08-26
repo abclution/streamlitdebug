e846a923ffa4e79fca32e5742e238fd98a56d276
abclution <andreas.tyrosvoutis@gmail.com>
2023-08-26 14:26:52 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,4 @@
 #hi
-
-
 import streamlit as st
 import pandas as pd
 import numpy as np
@@ -15,3 +13,32 @@ st.line_chart(
     y = ['col2', 'col3'],
     color = ['#FF0000', '#0000FF']  # Optional
 )
+
+energyUsageYearlyKwH_BTC = 2354234523456
+energyUsageYearlyKwH_BCH = 237645675673543
+# Define the range of values
+start_value = 0.007
+end_value = 0.35
+step = 0.001
+
+# Define the variable for energy usage
+#energyUsageYearlyKwH = 1000  # Example value, replace with your actual value
+
+# Create a list of values for column 1
+costPerKwH = [round(start_value + i * step, 3) for i in range(int((end_value - start_value) / step) + 1)]
+
+# Create a dictionary to hold the data
+data = {
+    "Electricity Cost (per KwH)": costPerKwH,
+    "Yearly, BTC Security Budget": [value * energyUsageYearlyKwH_BTC for value in costPerKwH],
+    "Yearly, BCH Security Budget": [value * energyUsageYearlyKwH_BCH for value in costPerKwH]
+}
+
+df = pd.DataFrame(data)
+
+ st.line_chart(
+     df,
+     x='Electricity Cost (per KwH)',
+     y=['Yearly, BTC Security Budget','Yearly, BCH Security Budget'],
+     color=['#F2A900', '#0AC18E']
+     )
